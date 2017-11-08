942b0bcd20e54c9802848f3219a7433d03f7a173
Ximin Luo <infinity0@debian.org>
2017-11-08 17:48:02 +0100

    Actually I was confused on what triple-compilation is, whoops

--- doublecompile.sh
+++ doublecompile.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
 # Call with no arguments to double-compile.
-# Call with "3" as the first arg to double- and triple-compile.
+# Call with "3" as the first arg to triple-compile, and check that this
+# reaches a fixed point. Exit non-0 if it's not, even if the build succeeded.
 #
-# Exit 0 if no differences from stage 2 onwards.
-# Exit >0 if there were differences on stage 2 or any later stage.
 
 prefix="$PWD/tcc-root" # constant prefix for now
 
@@ -26,22 +25,18 @@ if [ -z "$CC" ]; then
 	export CC=gcc
 fi
 
-./buildonce.sh "$prefix" build-0
+./buildonce.sh "$prefix" build-1
 
 unset CC CFLAGS LDFLAGS
 
-ln -sfT build-0 "$prefix"
-CC="$prefix/bin/tcc" ./buildonce.sh "$prefix" build-1
-sums build-1/
-
 ln -sfT build-1 "$prefix"
 CC="$prefix/bin/tcc" ./buildonce.sh "$prefix" build-2
-checkdiff build-1/ build-2/ "Double compilation reached fixed-point!"
+sums build-2/
 
 if [ "$1" = 3 ]; then
 
 ln -sfT build-2 "$prefix"
 CC="$prefix/bin/tcc" ./buildonce.sh "$prefix" build-3
-checkdiff build-2/ build-3/ "Double and triple compilation both reached fixed-point!"
+checkdiff build-2/ build-3/ "Triple compilation reached fixed-point!"
 
 fi
