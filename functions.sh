70ba8ac5f8018894a37185d7229ce2a5132a570a
Ximin Luo <infinity0@debian.org>
2017-11-08 23:11:02 +0100

    functions: checksum the checksums, for easier comparison

--- functions.sh
+++ functions.sh
@@ -1,6 +1,8 @@
-sums() {
-	find "$1" -type f -exec sha256sum '{}' \;
-}
+sums() {(
+	cd "$1"
+	find . -type f -printf '%P\0' | sort -z | xargs -r0 sha256sum | \
+	  { tee /dev/fd/3 | sha256sum; } 3>&1
+)}
 
 checkdiff() {
 	if diff -ru "$1" "$2"; then
