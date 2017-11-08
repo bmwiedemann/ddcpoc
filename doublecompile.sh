6ef42fec3b13d3aa5fd644b4df0d472a5bbfd63b
Ximin Luo <infinity0@debian.org>
2017-11-08 17:44:35 +0100

    Use diff(1) first in case they don't have diffoscope, and print hashes

--- doublecompile.sh
+++ doublecompile.sh
@@ -7,6 +7,20 @@
 
 prefix="$PWD/tcc-root" # constant prefix for now
 
+sums() {
+	find "$1" -type f -exec sha256sum '{}' \;
+}
+
+checkdiff() {
+	if diff -ru "$1" "$2"; then
+		sums "$2"
+		shift 2
+		echo >&2 "$@"
+	else
+		diffoscope --exclude-directory-metadata --html-dir "$2.diff" "$1" "$2"
+	fi
+}
+
 if [ -z "$CC" ]; then
 	echo >&2 "setting CC=gcc"
 	export CC=gcc
@@ -18,18 +32,16 @@ unset CC CFLAGS LDFLAGS
 
 ln -sfT build-0 "$prefix"
 CC="$prefix/bin/tcc" ./buildonce.sh "$prefix" build-1
+sums build-1/
 
 ln -sfT build-1 "$prefix"
 CC="$prefix/bin/tcc" ./buildonce.sh "$prefix" build-2
-diffoscope --exclude-directory-metadata --html-dir build-2.diff build-1/ build-2/
-echo >&2 "Double compilation reached fixed-point!"
+checkdiff build-1/ build-2/ "Double compilation reached fixed-point!"
 
 if [ "$1" = 3 ]; then
 
 ln -sfT build-2 "$prefix"
 CC="$prefix/bin/tcc" ./buildonce.sh "$prefix" build-3
-diffoscope --exclude-directory-metadata --html-dir build-3.diff build-2/ build-3/
-
-echo >&2 "Double and triple compilation both reached fixed-point!"
+checkdiff build-2/ build-3/ "Double and triple compilation both reached fixed-point!"
 
 fi
